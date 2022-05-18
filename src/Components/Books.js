6a9eec237c65a80c930371a749e9ff52ba5b70eb
jahid <info.jahidulislamjahid@gmail.com>
2022-05-18 15:10:08 +0600

    minor polish

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -4,24 +4,25 @@ import { addToReadingList } from '../Redux/Slices/BookSlice';
 import '../Styles/Books.scss';
 
 const Books = (props) => {
-    const {title, author, coverImageUrl, Synopsis} = props.books;
+    const {title, author, coverImageUrl, synopsis, pageCount} = props.books;
     const dispatch = useDispatch();
     return (
-        <>
+        <div>
             <div className="card">
                 <div className="card-header">
                     <img src={coverImageUrl} alt="Books images" />
                 </div>
                 <div className="card-body">
                         <h3>{title}</h3>
-                        <h4>{author}</h4>
-                        <p>{Synopsis}</p>
+                        <h4>BY: {author}</h4>
+                        <h5>Page: {pageCount}</h5>
+                        <p>{synopsis.slice(0,100)}...</p>
                     <div className="cardButton">
                         <button onClick={()=> dispatch(addToReadingList(props.books))}>Add To Reading list</button>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
