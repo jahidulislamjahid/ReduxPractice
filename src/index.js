bdf76e8f743f188f4b11dcd01e14f263e5c76c69
jahid <info.jahidulislamjahid@gmail.com>
2022-05-30 00:58:21 +0600

    persist redux implemented

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Home from './Components/Home';
 import { Provider } from 'react-redux';
 import { store} from './Redux/Store.js';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -21,7 +20,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
